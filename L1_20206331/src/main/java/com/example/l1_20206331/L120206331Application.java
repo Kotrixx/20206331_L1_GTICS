b1d22341f0bd58b48a4a95ee4f7d6dff34249d99
kotrix <a20206331@pucp.edu.pe>
2023-08-24 20:58:49 -0500

    subida de archivo

---
+++ L1_20206331/src/main/java/com/example/l1_20206331/L120206331Application.java
@@ -0,0 +1,13 @@
+package com.example.l1_20206331;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L120206331Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L120206331Application.class, args);
+    }
+
+}
---
+++ L1_20206331/src/test/java/com/example/l1_20206331/L120206331ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l1_20206331;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L120206331ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
