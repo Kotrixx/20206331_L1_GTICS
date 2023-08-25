442e08815403bfb0411c2bb48c62e8ab80706d74
kotrix <a20206331@pucp.edu.pe>
2023-08-24 21:53:36 -0500

    creacion de la ruta con parametro code

--- L1_20206331/src/main/java/com/example/l1_20206331/controller/HomeController.java
+++ L1_20206331/src/main/java/com/example/l1_20206331/controller/HomeController.java
@@ -4,13 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value ="/principal")
 public class HomeController {
 
+
     @GetMapping(value = "")
-    public String principal(){
-        return "index";
+    public String principal(@RequestParam("code") String code){
+        if(code.equals(null)){
+            return "index";
+        }
+        else{
+            return "indexPucp";
+        }
     }
+
+
 }
